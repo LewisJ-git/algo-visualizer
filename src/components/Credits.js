51a6a9f417e20fe98688f1b4b064b39762f5e574
LongNguyen-git <lucasnguyen2112@gmail.com>
2021-06-15 13:52:25 -0500

    Updated github link

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,10 +5,10 @@ const Credits = () => {
     return (
         <div className='Credits'>
             <div className='link-wrap'>
-                <a href='https://github.com/LewisJ-git' className='git-svg' target="_blank">
+                <a href='https://github.com/LewisJ-git/algo-visualizer' className='git-svg' target="_blank">
                     <GitIcon />
                 </a>
-                <a href='https://github.com/LewisJ-git' target="_blank">
+                <a href='https://github.com/LewisJ-git/algo-visualizer' target="_blank">
                     Fork me on GitHub!
                 </a>
             </div>
