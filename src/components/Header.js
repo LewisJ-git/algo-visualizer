dde5895b7ae324dac8d94c7133afbc65ca044e2d
LongNguyen-git <lucasnguyen2112@gmail.com>
2021-08-28 01:27:55 -0500

    Removed filler lines

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { ReactComponent as Headersvg1 } from '../svg/headersvg1.svg';
 import { ReactComponent as Headersvg2 } from '../svg/headersvg2.svg';
 
-// fill="#161616"
-
 const Header = () => {
-    const fillC = 'none', strokeCol = '#6c6c6c', strokeWid = '4';
     return (
         <div className = 'Header'> 
             <h1>Algorithm Visualizer</h1>
